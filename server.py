da644ce5fe007de3fd65d2fe59d8876cd074c4ec
iamMihirT <mihirt1606@gmail.com>
2023-11-05 16:09:56 +0100

    serverfileupdate

--- server.py
+++ server.py
@@ -20,9 +20,9 @@ class TwitterScrapeRequest(BaseModel):
     query: str = "wildfire"
 
 @app.put("/twitter/scrape")
-async def scrape_telegram(request: TelegramScrapeRequest):
+async def scrape_twitter(request: TwitterScrapeRequest):
     datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    outfile = f"data_telegram_{request.channel}_{datetime}.csv"
+    outfile = f"data_twitter_{request.channel}_{datetime}.csv"
 
     print(f"{datetime} - Starting scraping {outfile}")
     start_time = time.time()
